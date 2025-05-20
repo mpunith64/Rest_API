7b244e736141193a18bf18c580b6ffa8ae345869
PUNITH M <mpunith64@gmail.com>
2025-05-20 20:47:42 +0530

    cleanup5

--- IAC/Azure.tf
+++ IAC/Azure.tf
@@ -1,6 +1,6 @@
 provider "azurerm" {
   features {}
-  subscription_id = "44beb8fa-6219-4265-bbc9-1d73754f6b1"
+  subscription_id = "44beb8fa-6219-4265-bbc9-1d73754f6b17"
 }
 resource "azurerm_resource_group" "rg" {
   name     = "my-resource-group"
